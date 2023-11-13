23fae55fc30b3266e008cd47a55a2c64f68b4b7d
Abhistroo <126189705+Abhistroo@users.noreply.github.com>
2023-11-13 12:23:55 +0530

    hola

--- app/src/main/java/com/example/myapplication/Aulia.kt
+++ app/src/main/java/com/example/myapplication/Aulia.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.AuliaBinding
+import com.example.myapplication.signin
+import com.google.firebase.auth.FirebaseAuth
 
 class Aulia : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: AuliaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.aulia)
+        binding = AuliaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.logout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,signin::class.java))
+        }
     }
 }
